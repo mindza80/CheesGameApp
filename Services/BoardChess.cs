52c659b868c04181f63f342ab903f8d2655a093e
MiroslavPukovski <miroslav.pukovski@gmail.com>
2022-08-13 15:00:26 +0300

    logic

--- Services/BoardChess.cs
+++ Services/BoardChess.cs
@@ -99,8 +99,9 @@
         }
         public static BoardLocation GetLocation(int row, int cell) =>
             (BoardLocation)((row * 10) + cell + 1);
+
         public static (int, int) GetXYFromLocation(BoardLocation location) =>
-            ((int)location / 10, ((int)location % 10) - 1);
+            ((int)location / 10, ((int)location % 10) -1);
     }
     public class SelectedPiece
     {
@@ -109,14 +110,14 @@
     }
     public enum BoardLocation
     {
-        A1 = 01, A2 = 02, A3 = 03, A4 = 04, A5 = 05, A6 = 06, A7 = 07, A8 = 08,
-        B1 = 11, B2 = 12, B3 = 13, B4 = 14, B5 = 15, B6 = 16, B7 = 17, B8 = 18,
-        C1 = 21, C2 = 22, C3 = 23, C4 = 24, C5 = 25, C6 = 26, C7 = 27, C8 = 28,
-        D1 = 31, D2 = 32, D3 = 33, D4 = 34, D5 = 35, D6 = 36, D7 = 37, D8 = 38,
-        E1 = 41, E2 = 42, E3 = 43, E4 = 44, E5 = 45, E6 = 46, E7 = 47, E8 = 48,
-        F1 = 51, F2 = 52, F3 = 53, F4 = 54, F5 = 55, F6 = 56, F7 = 57, F8 = 58,
-        G1 = 61, G2 = 62, G3 = 63, G4 = 64, G5 = 65, G6 = 66, G7 = 67, G8 = 68,
-        H1 = 71, H2 = 72, H3 = 73, H4 = 74, H5 = 75, H6 = 76, H7 = 77, H8 = 78
+        A8 = 08, B8 = 18, C8 = 28, D8 = 38, E8 = 48, F8 = 58, G8 = 68, H8 = 78,
+        A7 = 07, B7 = 17, C7 = 27, D7 = 37, E7 = 47, F7 = 57, G7 = 67, H7 = 77,
+        A6 = 06, B6 = 16, C6 = 26, D6 = 36, E6 = 46, F6 = 56, G6 = 66, H6 = 76,
+        A5 = 05, B5 = 15, C5 = 25, D5 = 35, E5 = 45, F5 = 55, G5 = 65, H5 = 75,
+        A4 = 04, B4 = 14, C4 = 24, D4 = 34, E4 = 44, F4 = 54, G4 = 64, H4 = 74,
+        A3 = 03, B3 = 13, C3 = 23, D3 = 33, E3 = 43, F3 = 53, G3 = 63, H3 = 73,
+        A2 = 02, B2 = 12, C2 = 22, D2 = 32, E2 = 42, F2 = 52, G2 = 62, H2 = 72,
+        A1 = 01, B1 = 11, C1 = 21, D1 = 31, E1 = 41, F1 = 51, G1 = 61, H1 = 71
     }
     public enum PieceWithColor
     {
